9e276fd109a44675bbe31705d178f895deba5e90
Anna <annas.wieclaw@gmail.com>
2019-06-03 11:18:16 +0100

    Add more test to cover different cases in feature tests

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -4,12 +4,28 @@
       expect(page).to have_content "Rock Paper" 
     end
 
-    scenario "player can win with rock" do
+    scenario "player can win with scissors" do
       sign_and_play
       select "Scissors", from: "movee"
       srand(344)
       click_button "Submit my choice"
       expect(page).to have_content "You actually won" 
+    end
+
+    scenario "player can draw" do
+      sign_and_play
+      select "Paper", from: "movee"
+      srand(344)
+      click_button "Submit my choice"
+      expect(page).to have_content "It is a draw" 
+    end
+
+    scenario "player can lose" do
+      sign_and_play
+      select "Rock", from: "movee"
+      srand(344)
+      click_button "Submit my choice"
+      expect(page).to have_content "You lost" 
     end
 
     scenario "player can play again" do
